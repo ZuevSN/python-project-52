01da17d35bbbbc1dd0cfad7f6fb0d42795a91436
Sergei Zuev <zuevsn86@gmail.com>
2024-11-09 18:32:14 +0300

    fix

--- task_manager/statuses/urls.py
+++ task_manager/statuses/urls.py
@@ -3,11 +3,14 @@ from task_manager.statuses.views import (
     StatusListView,
     StatusCreateView,
     StatusUpdateView,
-    StatusDeleteView
+    StatusDeleteView,
+    UniversalListView
 )
+from .models import Status
 
 urlpatterns = [
-    path('', StatusListView.as_view(), name='statuses'),
+#    path('', StatusListView.as_view(), name='statuses'),
+    path('', UniversalListView.as_view(model=Status), name='statuses'),
     path('create/', StatusCreateView.as_view(), name='create_status'),
     path('<int:pk>/edit/', StatusUpdateView.as_view(), name='edit_status'),
     path('<int:pk>/delete/', StatusDeleteView.as_view(), name='delete_status'),
--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -5,16 +5,56 @@ from django.views.generic import (
     UpdateView,
     DeleteView
 )
+from django.views import View
+
 from django.utils.translation import gettext_lazy as _
 from django.urls import reverse_lazy
 from .models import Status
-from .forms import StatusForm
+from .forms import StatusForm, StatusesForm, UniversalForm
 
 
-class StatusListView(ListView):
+class GenericListView(View):
+    model = None
+    fields = []
+    def get(self, request, *args, **kwargs):
+        instances = self.model.objects.all()
+        return render(request, 'dataframe_template.html', {
+            'instances': instances,
+            'fields': self.fields,
+        })
+
+class UniversalListView(View):
+    model = None
+    
+    def get(self, request, *args, **kwargs):
+        header = self.model._meta.verbose_name
+        objects = self.model.objects.all()
+        form = UniversalForm(self.model)
+        context = {
+            'header': header,
+            'objects': objects,
+            'form': form,
+        }
+        return render(request, 'dataframe_template.html', context)
+
+    @classmethod
+    def as_view(cls, **initkwargs):
+        model = initkwargs.pop('model', None)  # Извлекаем модель из аргументов
+        if model is not None:
+            cls.model = model  # Устанавливаем модель как атрибут класса
+        return super().as_view(**initkwargs)
+
+class StatusListView(UniversalListView):
     model = Status
-    template_name = 'statuses/status_list.html'
-    context_object_name = 'statuses'
+#class StatusListView(ListView):
+#    model = Status
+#    template_name = 'statuses/status_list.html'
+#    context_object_name = 'statuses'
+#class StatusListView(View):
+#    def get(self, request, *args, **kwargs):
+#        form = StatusesForm
+#        return render(request, 'dataframe_template.html', {'form': form})
+
 
 
 class StatusCreateView(CreateView):
